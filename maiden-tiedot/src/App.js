e4ead5a9a7432a2129a6f34e0856e1347f167db8
aherman21 <hermansalexander2@gmail.com>
2023-11-26 00:54:10 +0100

    2.18 and 2.19 done

--- maiden-tiedot/src/App.js
+++ maiden-tiedot/src/App.js
@@ -7,21 +7,45 @@ import CountryList from './components/CountryList';
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [countries, setCountries] = useState([]);
+    const [filteredCountries, setFilteredCountries] = useState([])
+
+    useEffect(() => {
+            axios
+                .get(`https://studies.cs.helsinki.fi/restcountries/api/all/`)
+                .then(response => {
+                    setCountries(response.data);
+                })
+                .catch(error => {
+                    if (error.response && error.response.status === 404) {
+                        setCountries([])
+                    } else {
+                        console.error('Error fetching data', error)
+                    }
+                })
+            }, []);
 
     useEffect(() => {
         if (searchTerm) {
-            axios
-                .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${searchTerm}`)
-                .then(response => {
-                    setCountries(response.data);
-                });
+            const searchResults = countries.filter(country =>
+                country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                setFilteredCountries(searchResults)
+        } else {
+            setFilteredCountries([])
         }
-    }, [searchTerm]);
+    }, [searchTerm, countries]);
+
+    const handleShowCountry = (countryName) => {
+        const countryToShow = countries.filter(country =>
+            country.name.common === countryName
+        )
+        setFilteredCountries(countryToShow)
+    }
 
     return (
         <div>
-            <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-            <CountryList countries={countries} />
+            <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}></SearchBar>
+            <CountryList countries={filteredCountries} onShowCountry={handleShowCountry}/>
         </div>
     );
 };
--- maiden-tiedot/src/components/CountryList.js
+++ maiden-tiedot/src/components/CountryList.js
@@ -1,14 +1,17 @@
 
 import React from 'react';
 
-const CountryList = ({ countries }) => {
+const CountryList = ({ countries, onShowCountry }) => {
     if (countries.length > 10) {
-        return <div>Please specify a more specific filter</div>;
+        return <div>Too many matches, Please specify a more specific filter</div>;
     } else if (countries.length > 1) {
         return (
             <ul>
                 {countries.map(country => (
-                    <li key={country.name}>{country.name}</li>
+                    <li key={country.name.common}>
+                        {country.name.common}
+                        <button onClick={() => onShowCountry(country.name.common)}>show</button>
+                        </li>
                 ))}
             </ul>
         );
@@ -16,18 +19,21 @@ const CountryList = ({ countries }) => {
         const country = countries[0];
         return (
             <div>
-                <h1>{country.name}</h1>
-                <p>Capital: {country.capital}</p>
+                <h1>Country name: {country.name.common}</h1>
+                <p>Capital: {country.capital[0]}</p>
+                <p>Area: {country.area}</p>
                 <p>Population: {country.population}</p>
-                <img src={country.flag} alt={`Flag of ${country.name}`} width="200" />
+                <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
                 <h2>Languages</h2>
                 <ul>
-                    {country.languages.map(language => (
-                        <li key={language.name}>{language.name}</li>
+                    {Object.values(country.languages).map((language, index) => (
+                        <li key={index}>{language}</li>
                     ))}
                 </ul>
             </div>
         );
+    } else if (countries.length === 0) {
+        return <div>No matches, try a different filter</div>
     }
 
     return null;
